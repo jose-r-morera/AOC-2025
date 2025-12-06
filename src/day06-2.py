8026dc2bd9ff9119210f73f598527a48b4fd7cdb
jr <alu0101471846@ull.edu.es>
2025-12-06 06:03:11 +0000

    Day 6

--- src/day06-2.py
+++ src/day06-2.py
@@ -20,7 +20,9 @@ INPUT_FILE = DATA_DIR / "day06-input.txt"
 # ---------------------------------------------------------
 # Auxiliar Functions
 # ---------------------------------------------------------
-
+def read_vertical_number(matrix: list[list[str]], col: int) -> str:
+    """Reads a vertical number from the matrix at the given column."""
+    return "".join([matrix[row][col] for row in range(1, len(matrix) -1) if str.isdigit(matrix[row][col])])
 # ---------------------------------------------------------
 # Core Logic
 # ---------------------------------------------------------
@@ -28,25 +30,29 @@ def main(args: list[str]) -> int:
     """Main entry point for the script."""
 
     with open(INPUT_FILE, "r") as file:
-        matrix = [list(line.strip().split()) for line in file]
+        matrix = [list(line) for line in file]
+
+    # Make all operators have the same format / right limit
+    matrix[-1].append(" ")
+    matrix[-1].append("\n")
 
     result = 0
-    print("Matrix loaded:")
-    for col in range(len(matrix[0])):
-        operator =  matrix[-1][col]
-        total = int(matrix[0][col])
-        for row in range(1, len(matrix) - 1):
-            value = int(matrix[row][col])
-            if operator == "+":
-                total += value
-            elif operator == "-":
-                total -= value
-            elif operator == "*":
-                total *= value
-            elif operator == "/":
-                total /= value
-        result += total
-        #print (f"Column {col}: {total}")
+    segmet_start = 0
+    for segment_end in range(1, len(matrix[-1])):
+        if matrix[-1][segment_end] == " ":
+            continue
+        else: 
+            current_len = segment_end - segmet_start -1
+            operator = matrix[-1][segmet_start]
+            operation_total = 1 if operator == "*" else 0
+            for col in range(segmet_start, segmet_start+current_len):
+                number = matrix[0][col] if matrix[0][col] != " " else ""
+                number = number + read_vertical_number(matrix, col)
+                if operator == "+": operation_total += int(number)
+                elif operator == "*": operation_total *= int(number)
+            result += operation_total
+
+            segmet_start = segment_end
 
     print(f"Result: {result}")
 
